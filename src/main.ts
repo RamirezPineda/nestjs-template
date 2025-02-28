3c173d478563cb56572688fb426f4de80a696b19
RamirezPineda <ramirezpineda.rickyroy@gmail.com>
2025-02-28 09:53:55 -0400

    chore: update bootstrap config with cors and validation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+import { corsConfig } from '@/config/cors.config';
+import { AppModule } from '@/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const logger = new Logger('bootstrap');
+
+  app.setGlobalPrefix('api/v1');
+  app.enableCors(corsConfig);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  const configService = app.get(ConfigService);
+  const port: number = configService.get('PORT') ?? 4000;
+  const appUrl: string =
+    configService.get('APP_URL') ?? 'http://localhost:4000';
+
+  await app.listen(port);
+  logger.log(`Server running in ${appUrl} 🚀`);
 }
-bootstrap();
+void bootstrap();
