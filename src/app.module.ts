f6ddcc151da9e25524eedd7b6d6343075965013e
RamirezPineda <ramirezpineda.rickyroy@gmail.com>
2025-02-28 17:40:58 -0400

    feat: add typeorm and common module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { appConfig } from '@/config/app.config';
+import { dataSourceConfig } from '@/config/data-source.config';
+import { CommonModule } from '@/common/common.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(appConfig)],
+  imports: [
+    ConfigModule.forRoot(appConfig),
+    TypeOrmModule.forRoot(dataSourceConfig),
+    CommonModule,
+  ],
 })
 export class AppModule {}
