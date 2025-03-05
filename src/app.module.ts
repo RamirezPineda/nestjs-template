6f3f131e3a7c423cb6c8665bd8e2032df6508786
RamirezPineda <ramirezpineda.rickyroy@gmail.com>
2025-03-05 17:47:29 -0400

    feat: add users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { appConfig } from '@/config/app.config';
 import { dataSourceConfig } from '@/config/data-source.config';
 import { CommonModule } from '@/common/common.module';
+import { UsersModule } from '@/users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(appConfig),
     TypeOrmModule.forRoot(dataSourceConfig),
     CommonModule,
+    UsersModule,
   ],
 })
 export class AppModule {}
